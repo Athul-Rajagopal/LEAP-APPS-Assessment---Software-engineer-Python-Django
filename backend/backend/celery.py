8c3a79627cd9e44f19c7d1d5b414fc92d98c32e0
Athul Rajagopalan P <athulrajagopalan88@gmail.com>
2024-04-05 12:29:07 +0530

    changes made in celery.py module in the app creation.

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -2,12 +2,15 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from decouple import config
+from django.conf import settings
 
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-app = Celery('backend')
+app = Celery('backend',
+             broker_url=settings.CELERY_BROKER_URL,
+             backend=settings.CELERY_RESULT_BACKEND )
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
